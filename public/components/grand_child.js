4e3d4e32466419a2f61eab9094e9c97ee49b7ab8
uhuru_yokota_eiji <e.yokota@uhuru.jp>
2020-07-30 09:48:39 +0900

    use reducer

--- public/components/grand_child.js
+++ public/components/grand_child.js
@@ -1,11 +1,26 @@
-import {useState} from "react"
+import {useReducer} from "react"
+
+const initialState = {count: 0};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
 
 export default function GrandChildComponent() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {count:0});
   return (
     <>
-    <h1>GrandChildComponent</h1>
-    <p>count : {count}</p>
+      <h1>GrandChildComponent</h1>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
     </>
   );
 }
